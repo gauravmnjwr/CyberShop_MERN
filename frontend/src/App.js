2fb6fb76242e5368f43251995185dddabf6ac636
gauravmnjwr <munjewargaurav@gmail.com>
2023-05-08 16:00:28 +0530

    add user registration

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import { Container } from "react-bootstrap";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen";
+import LoginScreen from "./screens/LoginScreen";
 function App() {
   return (
     <Router>
@@ -12,9 +13,10 @@ function App() {
       <main className="py-3">
         <Container>
           <Routes>
-            <Route path="/" element={<HomeScreen />} exact />
+            <Route path="/login" element={<LoginScreen />} />
             <Route path="/product/:id" element={<ProductScreen />} />
             <Route path="/cart/:id?" element={<CartScreen />} />
+            <Route path="/" element={<HomeScreen />} exact />
           </Routes>
         </Container>
       </main>
